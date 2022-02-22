aea8629446efa0d13c66b4c5137230ed468804ec
LoopAnomaly <mcle5738@vandals.uidaho.edu>
2022-02-22 03:47:33 -0800

    Update player movement
    Moved the player script to my folder to continue work on it for the MVP. Altered the code that handles basic movement in order to provide better collisions

---
+++ Assets/src/Toby/playerClass.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerClass : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    Rigidbody2D rgdb;
+    Vector2 newPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgdb =  this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos = new Vector2(this.transform.position.x, this.transform.position.y);
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            newPos.x += (moveSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+        }
+
+        if(Input.GetAxisRaw("Vertical") != 0)
+        {
+            newPos.y += (moveSpeed * Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        }
+        
+        rgdb.MovePosition(newPos);
+        this.transform.rotation = Quaternion.identity;
+    }
+}
