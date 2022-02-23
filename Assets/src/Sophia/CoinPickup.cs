79910d45fa9ad9d9418aee06e4603e03ef3ff611
sophiasivula <sivu7048@vandals.uidaho.edu>
2022-02-23 10:09:41 -0800

    Alpine-Chronicles

--- Assets/src/Sophia/CoinPickup.cs
+++ Assets/src/Sophia/CoinPickup.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class CoinPickup : MonoBehaviour
 {
+
+    public AudioSource sound;
     void OnTriggerEnter2D (Collider2D col){
         if(col.gameObject.name == "Player"){
             Debug.Log("Coin has been collected!");
+            sound.Play();
             Destroy(this.gameObject);
+
         }
+
 
     }
 }
