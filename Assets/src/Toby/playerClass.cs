b1916fe18e7d66b3ff42c45bec9f2e7e0443847b
LoopAnomaly <mcle5738@vandals.uidaho.edu>
2022-02-22 21:45:18 -0800

    Added player/computer interaction
    Added interaction between the player and the computer for the MVP. Later, the interactables may need to be implemented as an interface possible. Or at the very least, the player will have to have more generic and loosely coupled interaction than the hard coded "computer" interaction.

--- Assets/src/Toby/playerClass.cs
+++ Assets/src/Toby/playerClass.cs
@@ -7,12 +7,14 @@ public class playerClass : MonoBehaviour
     [SerializeField] float moveSpeed;
     Rigidbody2D rgdb;
     Vector2 newPos;
+    int interacted = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         rgdb =  this.GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -32,5 +34,53 @@ public class playerClass : MonoBehaviour
         this.transform.rotation = Quaternion.identity;
     }
 
-    
+    // private void FixedUpdate()
+    // {
+    //     if(Input.GetAxisRaw("Horizontal") != 0)
+    //     {
+    //         this.transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f));
+    //     }
+
+    //     if(Input.GetAxisRaw("Vertical") != 0)
+    //     {
+    //         this.transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
+    //     }
+    // }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "item")
+        {
+            pickupItem(other.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name != "Computer")
+        {
+            return;
+        }
+        
+        if (interacted == 1)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Player has interacted with the computer");
+            interacted = 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        interacted = 0;    
+    }
+
+    private void pickupItem(GameObject item)
+    {
+        Debug.Log("Picked up an item named " + item.name);
+    }
 }
