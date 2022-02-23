14900017e68cce3262b3981701b5e4f9c6b5193e
WoodyEric <woodard.eric09@gmail.com>
2022-02-23 15:17:00 -0800

    Write to console when PlaySound is called

--- Assets/src/Eric/AudioPlayer.cs
+++ Assets/src/Eric/AudioPlayer.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
-    private float volume = 0.5f;
+    private float volume = 0.2f;
 
     public AudioSource audioSource;
 
-    public void Play()
-    { 
+    public void PlaySound()
+    {
         audioSource.volume = volume;
         audioSource.Play();
+        Debug.Log("Sound Played!");
     }
 }
