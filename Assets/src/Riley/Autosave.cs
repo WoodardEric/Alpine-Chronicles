6f369f7c9c05c1dfa1f7772511faf508a22e328c
Riley Walsh <rewalsh@nic.edu>
2022-02-26 18:29:18 -0800

    added player data

--- Assets/src/Riley/Autosave.cs
+++ Assets/src/Riley/Autosave.cs
@@ -4,40 +4,55 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary; 
+
+
 public class Autosave : MonoBehaviour
 {
 	public float Timer = 0;
 	public bool SaveGame = false;
-	public float Timecheck = 5f;
+	public float Timecheck = 1800f;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		Timer = Timer + 1 * Time.deltaTime;
-	}
 
+		//loop auto saveusing coroutine if needed
+		//StartCoroutine(Countdown3());
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		Timer = Timer + 1 * Time.deltaTime;
-		if (Timer >= Timecheck)
-		{
-			SaveGame = true;
+			Timer = Timer + 1 * Time.deltaTime;
+			if (Timer >= Timecheck)
+			{
+				SaveGame = true;
 
-		}
+			}
 
-		if (SaveGame == true)
-		{
-			SaveGameFunc();
+			if (SaveGame == true)
+			{
+			Debug.Log("AutoSaving Data...");
+			//SavePlayerFunc(playerClass);
 			Timer = 0f;
-			
 
-		}
-
+			}
 	}
-
-	public static void SaveGameFunc() //insert player data here
+	
+	//coroutine if using will continue to loop forever. 
+	/*
+	private IEnumerator Countdown3()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(3); //wait 3 seconds
+												//do other thing
+			Debug.Log("AutoSaving Data...");
+		}
+	}*/
+	
+	public static void SavePlayerFunc(playerClass pplayer) //insert player data here
 
 	{
 		//save the data in binary, more secure
@@ -46,14 +61,15 @@ public class Autosave : MonoBehaviour
 		string path = Application.persistentDataPath + "/data.ap";
 		FileStream stream = new FileStream(path, FileMode.Create);
 
-		//save player data here
-		//save level data here
-		Debug.Log("AutoSaving Data...");
+		PlayerData data = new PlayerData(pplayer);
+		formatter.Serialize(stream, data);
+		
 		Debug.Log(Application.persistentDataPath);
 		stream.Close();
 		
-
 	}
+
+
 
 
 }
---
+++ Assets/src/Riley/PlayerData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float[] position;
+
+    public PlayerData (playerClass player)
+    {
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+
+    }
+    
+}
