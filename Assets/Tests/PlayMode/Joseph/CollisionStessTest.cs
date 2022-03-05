f76e34e2a53be359d369a43c75009bfd3e2a2e4a
Joseph Miller <jcmiller710@gmail.com>
2022-03-05 08:53:41 -0800

    I have no idea how to write tests.

--- Assets/Tests/PlayMode/Joseph/CollisionStessTest.cs
+++ Assets/Tests/PlayMode/Joseph/CollisionStessTest.cs
@@ -6,19 +6,39 @@ using UnityEngine.TestTools;
 
 public class CollisionStressTest
 {
-   // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-   // `yield return null;` to skip a frame.
-   [UnityTest]
-   public IEnumerator WallCollision()
-   {
+  // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+  // `yield return null;` to skip a frame.
+  [UnityTest]
+  public IEnumerator WallCollision()
+  {
+      //public GameObject brick;
+      //GameObject brick = new GameObject();
+      //brick.AddComponent<BoxCollider2D>();
+      //brick.transform.position = new Vector2(0f,4f);
       GameObject gameObject = new GameObject();
-      Vector3 intialPos = new Vector3(0f,-4f,0f);
+      gameObject.AddComponent<Rigidbody2D>();
+      gameObject.AddComponent<BoxCollider2D>();
+      gameObject.GetComponent<Rigidbody2D>().gravityScale=0f;
+      Vector2 intialPos = new Vector2(0f,3f);
       gameObject.transform.position = intialPos;
+      yield return null;
+      float speed=1f;
 
       //Act
-      
+     //while(gameObject.transform.position.y < 4f)
+     //{
+         Vector2 newPos = new Vector2(0f,speed+intialPos.y);
+         gameObject.GetComponent<Rigidbody2D>().MovePosition(newPos);
+         yield return null;
+         Debug.Log("Speed = "+speed);
+         speed = speed * 1.05f;
+     //}
+
       // Use the Assert class to test conditions.
+      Vector3 expected = new Vector3(0f,4f,0f);
+      Assert.AreEqual(expected,gameObject.transform.position);
+
       // Use yield to skip a frame.
       yield return null;
-   }
+  }
 }
