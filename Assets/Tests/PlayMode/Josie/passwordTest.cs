f753213b0d339d76eadc399c44ca302afa187a9f
LoopAnomaly <mcle5738@vandals.uidaho.edu>
2022-03-07 20:03:31 -0800

    Fixed player error in boundary test

--- Assets/Tests/PlayMode/Josie/passwordTest.cs
+++ Assets/Tests/PlayMode/Josie/passwordTest.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 public class passwordTest
 {
     bool valid;
     // A Test behaves as an ordinary method
-    [Test]
-    public void passwordTestSimplePasses()
+    [UnityTest]
+    public IEnumerator passwordTestSimplePasses()
     {
+        SceneManager.LoadScene("PassTestScene");
         // Use the Assert class to test conditions
         GameObject obj = new GameObject();
-        obj.AddComponent<PlayerClass>();
+        //obj.AddComponent<PlayerClass>();
         drbcMode t = obj.AddComponent<drbcMode>();
+
+        yield return null;
 
         valid = t.readStringInput("s");
         Assert.IsTrue(valid);
