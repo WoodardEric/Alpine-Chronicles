130d43b490759a6b5efdb361c2d25f46cee40d19
Joseph Miller <jcmiller710@gmail.com>
2022-03-14 10:48:55 -0700

    More Level_1 design

--- Assets/src/Joseph/Void.cs
+++ Assets/src/Joseph/Void.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Void : MonoBehaviour
 {
    public int triggered = 0;
+   public int scene;
+   public int damage = -5;
 
    // Start is called before the first frame update
    void Start(){}
@@ -14,6 +16,22 @@ public class Void : MonoBehaviour
    void OnTriggerStay2D(Collider2D other)
    {
       triggered=1;
+      Vector2 position = new Vector2(0f,0f);
+      PlayerClass player = PlayerClass.Instance;
+      if(other.name != "Player")
+      {
+         return;
+      }
+
+      //Move Player to the start of the level
+      if(scene == 2)
+      {
+         position.x = -9f;
+         position.y = 3.75f;
+         player.setPlayerPos(position);
+      }
+      //Damage the player
+      player.updateHealth(damage);
    }
 
    void OnTriggerExit2D(Collider2D other)
