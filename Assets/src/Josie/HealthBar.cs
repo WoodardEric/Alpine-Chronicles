73afc6294e702876bc78e687faa41bb894248c7c
josiewicklund <70339516+josiewicklund@users.noreply.github.com>
2022-03-28 09:51:03 -0700

    Implemented Health Bar

---
+++ Assets/src/Josie/HealthBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public PlayerClass player = null;
+
+    //Start
+    void Start()
+    {
+        player = PlayerClass.Instance;  
+    }
+    
+    //Update
+    void Update()
+    {
+        int i = player.GetHealth();
+        Debug.Log("got health");
+        SetHealth(i);
+    }
+    
+    //
+    public void setMaxHealth(int health)
+    {
+        slider.maxValue = health; 
+        slider.value = health;
+    }
+
+    //move slider on health bar 
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    } 
+}
