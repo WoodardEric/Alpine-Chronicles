f22e6fe24bbc45439b119fc7ded7503649331875
Riley Walsh <rewalsh@nic.edu>
2022-03-29 10:52:52 -0700

    Reorganized savescript/autosave

--- Assets/src/Riley/SaveScript.cs
+++ Assets/src/Riley/SaveScript.cs
@@ -8,78 +8,69 @@ using UnityEngine.SceneManagement;
 
 public class SaveScript : MonoBehaviour
 {
-    //public int healthy;
     public PlayerClass player = null;
-    public Slider slider;
-    //public HealthBar Slider; 
-    // Start is called before the first frame update
+    
     void Start()
     {
-       
         player = PlayerClass.Instance;
         //healthy = myObject.GetHealth();
         //PlayerPrefs.GetInt("health");
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        //int i = player.GetHealth();
-        //SetHealth(i);
-        //int sethealth = player.GetHealth();
-        //savelevel(sethealth);
+       
 
     }
+
 
     public void Create()
     { 
     
-   
     
     }
+
+
+    public void SaveGame()
+    {
+        savelevel(); 
+        //public int healthy = player.health;
+        int healthy = player.GetHealth();
+        PlayerPrefs.SetInt("health", healthy);
+        Debug.Log("saving..." + healthy);
+
+    }
+
+
+    public void LoadGame()
+    {
+        LoadLevel(); 
+        int healthy = PlayerPrefs.GetInt("health");
+        player.health = healthy;
+        //player.UpdateHealth(healthy);
+        //healthy =  PlayerPrefs.GetInt("health");
+        Debug.Log("Loading..." + healthy);
+    }
+
 
     public void savelevel()
     {   //Get active scene
         //PlayerClass myObject = GameObject.FindObjectOfType<PlayerClass>();
         PlayerClass myObject = GameObject.FindObjectOfType<PlayerClass>();
         PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
-        //public int healthy = player.health;
-        int healthy = player.GetHealth();
-        PlayerPrefs.SetInt("health",healthy);
-       
-        Debug.Log("saving..."+healthy);
     }
 
 
-    public void LoadNumber()
+    public void LoadLevel()
     {
         PlayerClass myObject = GameObject.FindObjectOfType<PlayerClass>();
         SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
         PlayerClass player = PlayerClass.Instance;
         player.IsInteracting(false);
         player.SetPlayerPos(new Vector2(-5.18f, -2.87f));
-
-        int healthy = PlayerPrefs.GetInt("health");
-        //player.UpdateHealth(healthy);
-        player.health = healthy; 
-        //healthy =  PlayerPrefs.GetInt("health");
-        Debug.Log("Loading..." + healthy);
-      
     }
-
-    public void setMaxHealth(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
-
-    //move slider on health bar 
-    public void SetHealth(int health)
-    {
-        slider.value = health;
-    }
-
-
 
 
 }
