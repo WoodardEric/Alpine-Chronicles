88173ac9c0548ba9630908b2930a33c3e9d9fa9c
LoopAnomaly <mcle5738@vandals.uidaho.edu>
2022-04-07 09:50:20 -0700

    Fixed Unity/Unix autosave bug

---
+++ Assets/src/Riley/AutoSave.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
+
+public class AutoSave : MonoBehaviour
+{
+    public PlayerClass player = null;
+
+    void Start()
+    {
+        player = PlayerClass.Instance;
+    }
+
+    public void SavePlayer()
+    {
+        int healthy = player.GetHealth();
+        PlayerPrefs.SetInt("health", healthy);
+        PlayerPrefs.SetInt("score", player.GetScore());
+        Vector2 pos = player.GetPos();
+        PlayerPrefs.SetFloat("xPos", pos.x);
+        PlayerPrefs.SetFloat("yPos", pos.y);
+        Debug.Log("Saving...");
+    }
+
+    public void SaveLevel()
+    {   
+        PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
