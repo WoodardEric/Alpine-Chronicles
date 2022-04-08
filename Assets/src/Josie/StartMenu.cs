c678f2f0839acaa4a2742f9ef71306a3567d8ecd
josiewicklund <70339516+josiewicklund@users.noreply.github.com>
2022-04-08 11:07:09 -0700

    Added main menu stuff

--- Assets/src/Josie/StartMenu.cs
+++ Assets/src/Josie/StartMenu.cs
@@ -6,26 +6,27 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     PlayerClass player = null;
-        public void PlayGame()
-        {
-            player.IsInteracting(false);
-            //to load the next scene in the queue
-            Debug.Log("change to level_0 scene");
-            SceneManager.LoadScene("Level_0");
-            player.SetPlayerPos(new Vector2(-5.18f, -2.87f));
-            player.ResetPlayer();
-        }
+    public void PlayGame()
+    {
+        player.IsInteracting(false);
+        //to load the next scene in the queue
+        Debug.Log("change to level_0 scene");
+        SceneManager.LoadScene("Level_0");
+        player.SetPlayerPos(new Vector2(-5.18f, -2.87f));
+        player.ResetPlayer();
+    }
 
-        public void drBcMode()
-        {
-            Debug.Log("pull up password input text");
-            SceneManager.LoadScene("inputPassword");
-        }
+    public void drBcMode()
+    {
+        Debug.Log("pull up password input text");
+        SceneManager.LoadScene("inputPassword");
+    }
 
-        public void LoadGame()
-        {
-            SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
-        }   
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
+    }   
+    
     // Start is called before the first frame update
     void Start()
     {
