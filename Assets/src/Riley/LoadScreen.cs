709f6b216d2f58e7e08b651fe82cc75bbbc3ac69
Riley Walsh <rewalsh@nic.edu>
2022-04-09 21:18:01 -0700

    Added loading screen

--- Assets/src/Riley/LoadScreen.cs
+++ Assets/src/Riley/LoadScreen.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScreen : MonoBehaviour
 {
+    public GameObject LoadingScreen;
+    public Slider slider;
     public SaveScript count; 
     // Start is called before the first frame update
     public void LoadLevel(int sceneIndex)
@@ -17,11 +20,13 @@ public class LoadScreen : MonoBehaviour
     {
         //sceneIndex = count.track;
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-
+        LoadingScreen.SetActive(true);
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
-            Debug.Log(progress);
+            //Debug.Log(progress);
+
+            slider.value = progress; 
             yield return null; 
         
         }
