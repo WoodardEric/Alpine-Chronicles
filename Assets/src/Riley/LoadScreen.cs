4389b5eba64789bea259db9159faeee6e241c70c
Riley Walsh <rewalsh@nic.edu>
2022-04-09 21:33:56 -0700

    Load screen done

--- Assets/src/Riley/LoadScreen.cs
+++ Assets/src/Riley/LoadScreen.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Threading;
+using System.IO;
 
 public class LoadScreen : MonoBehaviour
 {
     public GameObject LoadingScreen;
     public Slider slider;
-    public SaveScript count; 
+    public SaveScript count;
+    public Text progressText; 
     // Start is called before the first frame update
     public void LoadLevel(int sceneIndex)
     {
-        //sceneIndex = count.track; 
+        //sceneIndex = count.track;
+
+        
         StartCoroutine(LoadAsynchronously(sceneIndex));
         
+
     }
 
     IEnumerator LoadAsynchronously(int sceneIndex)
@@ -21,12 +27,15 @@ public class LoadScreen : MonoBehaviour
         //sceneIndex = count.track;
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
         LoadingScreen.SetActive(true);
+        
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
             //Debug.Log(progress);
+           
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
 
-            slider.value = progress; 
             yield return null; 
         
         }
