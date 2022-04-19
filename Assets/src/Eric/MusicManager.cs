2c3eb07eb2241481b3d520dd1faed7b576ec8c90
WoodyEric <woodard.eric09@gmail.com>
2022-04-18 17:08:52 -0700

    Finish updating documentation and style

--- Assets/src/Eric/MusicManager.cs
+++ Assets/src/Eric/MusicManager.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+* Summary: AudioManager subclass that controls playing music tracks for the game.
+* 
+* Member Variables:
+* tracks - a dictionary of all avialbe music tracks. Key is a MusicTrack enum that maps to a AudioClip value.
+* currentVolume - The audiosources current volume that can be used to restore the volume after a temporary fade.
+*/
 public class MusicManager : AudioManager
 {
     private readonly Dictionary<MusicTrack, AudioClip> tracks = new Dictionary<MusicTrack, AudioClip>();
     private float currentVolume;
 
+    /*
+    * Summary: Instance Property get and set the MusicManager instance for this singleton class.
+    * 
+    * Parameters:
+    * MusicManager - the current instance of a MusicManger class
+    * 
+    * return:
+    * MusicManger - returns the only allowed instance of a MusicManager class.
+    */
     public static MusicManager Instance 
     { 
         get; 
@@ -14,6 +30,11 @@ public class MusicManager : AudioManager
     }
 
 
+    /*
+    * Summary: initilize the the MusicManager to set the instance if
+    * creating for the first time. Else destory the MusicManager trying to
+    * be created.
+    */
     protected override void Awake()
     {
         if (Instance != null)
@@ -32,6 +53,13 @@ public class MusicManager : AudioManager
     }
 
 
+    /*
+    * Summary: Start playing a new track. If a track is currently being played,
+    * it will be faded out before the new track fades in
+    * 
+    * Parameters:
+    * track - New music track that will be played.
+    */
     public void Play(MusicTrack track)
     {
         var clip = tracks[track];
@@ -47,15 +75,27 @@ public class MusicManager : AudioManager
     }
 
 
+    /*
+    * Summary: Reads music files from the Music folder in the resources directory as clips and
+    * adds them to the tracks list.
+    * 
+    */
     protected override void ReadClips()
     {
         tracks.Add(MusicTrack.Menu, Resources.Load("Music/menu") as AudioClip);
-        tracks.Add(MusicTrack.SceneOne, Resources.Load("Music/menu") as AudioClip);
-        tracks.Add(MusicTrack.SceneTwo, Resources.Load("Music/menu") as AudioClip);
-        tracks.Add(MusicTrack.SceneThree, Resources.Load("Music/menu") as AudioClip);
+        tracks.Add(MusicTrack.SceneOne, Resources.Load("Music/SceneOne") as AudioClip);
+        tracks.Add(MusicTrack.SceneTwo, Resources.Load("Music/SceneTwo") as AudioClip);
+        tracks.Add(MusicTrack.SceneThree, Resources.Load("Music/SceneThree") as AudioClip);
     }
 
 
+    /*
+    * Summary: fades the music out for a set duration before
+    * fading back in
+    * 
+    * Parameters:
+    * duration - The time in seconds to fade the music for. 
+    */
     public void DuckMusic(float duration)
     {
         currentVolume = Volume;
@@ -65,24 +105,46 @@ public class MusicManager : AudioManager
         Invoke(nameof(ResetVolume), duration);
     }
 
-
+    /*
+    * Summary: fades the music out for a set duration before
+    * fading back in
+    */
     private void ResetVolume()
     {
         StartCoroutine(Fade(0.5f, currentVolume));
     }
 
 
+    /*
+    * Summary: Transitions the audio source to a new music track
+    * 
+    * Parameters:
+    * tack - The audio clip to be played
+    * 
+    * Return:
+    * IEnumerator -
+    */
     private IEnumerator TransitionTrack(AudioClip track)
     {
+        var currVolume  = Volume;
         StartCoroutine(Fade(0.5f, 0f));
         yield return new WaitForSeconds(0.48f);
         audioSource.Stop();
         audioSource.clip = track;
         audioSource.Play();
-        StartCoroutine(Fade(0.5f, Volume));
+        StartCoroutine(Fade(0.5f, currVolume));
     }
 
-
+    /*
+    * Summary: Fades the audio track over a given amount of time.
+    * 
+    * Parameters:
+    * dur - The length of time to perform the fade over.
+    * vol - The target volume of for the fade.
+    * 
+    * Return:
+    * IEnumerator -
+    */
     private IEnumerator Fade(float dur, float vol)
     {
         float currentTime = 0;
@@ -97,12 +159,9 @@ public class MusicManager : AudioManager
     }
 
 
-    void Update()
-    {
-
-    }
-
-
+    /*
+    * Summary: List of the avialbe music track that can be used in the game
+    */
     public enum MusicTrack
     {
         Menu,
--- Assets/src/Eric/SoundManager.cs
+++ Assets/src/Eric/SoundManager.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 
+/*
+* Summary: AudioManager sublcass that Controls playing all in-game sound effects
+* 
+* Member Variables:
+* clips - a dictionary of all avialbe music tracks. Key is a MusicTrack enum that maps to a Sound object
+*/
 public class SoundManager : AudioManager
 {
     private readonly Dictionary<SoundEffect, Sound> clips = new Dictionary<SoundEffect, Sound>();
 
 
+    /*
+    * Summary: Instance Property get and set the SoundManager instance for this singleton class.
+    * 
+    * Parameters:
+    * MusicManager - the current instance of a SoundManager class
+    * 
+    * return:
+    * MusicManger - returns the only allowed instance of a SoundManager class.
+    */
     public static SoundManager Instance 
     { 
         get; 
@@ -16,6 +31,11 @@ public class SoundManager : AudioManager
     }
 
 
+    /*
+    * Summary: initilize the the MusicManager to set the instance if
+    * creating for the first time. Else destory the MusicManager trying to
+    * be created.
+    */
     protected override void Awake()
     {
         if (Instance != null)
@@ -33,6 +53,12 @@ public class SoundManager : AudioManager
     }
 
 
+    /*
+    * Summary: Use to play a soundeffect in game
+    * 
+    * Parameters:
+    * soundeffect - soundeffect that will be played.
+    */
     public void Play(SoundEffect soundEffect)
     {
         Sound sound = clips[soundEffect];
@@ -54,6 +80,10 @@ public class SoundManager : AudioManager
     }
 
 
+    /*
+    * Summary: Loads all soundeffect files in the Sounds folder in the resources directory
+    * as a audioclip and adds it to the clips dictionary.
+    */
     protected override void ReadClips()
     {
         foreach (SoundEffect effect in Enum.GetValues(typeof(SoundEffect)))
@@ -63,6 +93,13 @@ public class SoundManager : AudioManager
     }
 
 
+    /*
+    * Summary: Get a random variant for a soundeffect. Used to add variation to
+    * common sounds like the attack sound.
+    * 
+    * Parameters:
+    * sound - the base sound to be played
+    */
     private AudioClip GetRandomVariant(SoundEffect sound)
     {
         var clipVariants = new List<AudioClip>();
@@ -77,6 +114,10 @@ public class SoundManager : AudioManager
         return clipVariants[Random.Range(0, clipVariants.Count)];
     }
 
+
+    /*
+    * Summary: List of the avialbe sound effects that can be used in the game
+    */
     public enum SoundEffect
     {
         Attack,
@@ -94,12 +135,24 @@ public class SoundManager : AudioManager
     }
 
 
+    /*
+    * Summary: A struct to organize flags for a soundeffect
+    * 
+    * Member Variables:
+    * clip - the audioclip that is played by an audio source
+    * IsVariant - true if the Sound has variants
+    * IsMusical - true if the Sound is a small musical piece that should duck the music
+    */
     private class Sound
     {
         public AudioClip clip;
         public bool IsVariant = false;
         public bool IsMusical = false;
 
+        /*
+        * Summary: Constructor to initilze all the member varialbes and read
+        * the soundeffect file into the clip
+        */
         public Sound(SoundEffect soundEffect)
         {
             if (soundEffect.ToString().Contains("Attack"))
